4dff032674741fc5d79bff221dc14af4c436b386
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-07-21 12:33:25 +0300

    basic getting started

---
+++ README.md
@@ -0,0 +1,1 @@
+# SoundPlayer Desktop (OS X)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-require('Common');
-var win = new Window();
+import 'Common';
+
+const win = new Window();
+
 application.exitAfterWindowsClose = true;
 win.visible = true;
 win.title = 'Hello World';
